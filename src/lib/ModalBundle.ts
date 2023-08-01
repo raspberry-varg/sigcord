d1f93b5df846ceda50a5f6ad5bd7148ee431db02
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2023-07-31 23:07:36 -0700

    added `useValues`
    This public method provides the user with the values from a modal submission to use in their modal submit callback.

--- src/lib/ModalBundle.ts
+++ src/lib/ModalBundle.ts
@@ -4,10 +4,37 @@ import type {
   ModalSubmitInteraction,
 } from 'discord.js';
 
+export type ModalBundle<Props = {}> = (props: Props) => ModalBundleBlueprint;
+
 export interface ModalBundleBlueprint {
   getModal: () => ModalBuilder;
   getSubmitOptions: () => AwaitModalSubmitOptions<ModalSubmitInteraction>;
-  getSubmitHandler: () => (response: ModalSubmitInteraction) => any;
+  getSubmitHandler: () => ModalSubmitHandler;
 }
 
-export type ModalBundle<Props = {}> = (props: Props) => ModalBundleBlueprint;
+type ModalSubmitHandler = (response: ModalSubmitInteraction) => unknown;
+type ModalSubmitHandlerWithValues<Values extends {}> = (
+  response: ModalSubmitInteraction,
+  values: Values
+) => unknown;
+
+function extractModalSubmitValues<Values extends {}>(
+  modalSubmit: ModalSubmitInteraction
+): Values {
+  const entries = Array.from(
+    modalSubmit.fields.fields.entries(),
+    ([key, { value }]) => {
+      return [key, value];
+    }
+  );
+  return Object.fromEntries(entries);
+}
+
+export function useValues<Values extends {}>(
+  modalSubmitHandler: ModalSubmitHandlerWithValues<Values>
+): ModalSubmitHandler {
+  return (response) => {
+    const values = extractModalSubmitValues<Values>(response);
+    return modalSubmitHandler(response, values);
+  };
+}
