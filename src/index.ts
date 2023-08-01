90becef93fca617037bb60d2d733ad18012d4495
Robert Vargas <90274287+robertvargas-irq@users.noreply.github.com>
2023-07-31 23:07:36 -0700

    added `useValues`

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ export * from './lib/MenuView';
 export * from './lib/Router';
 export * from './lib/SmartComponents';
 export * from './lib/PrebuiltEmbeds';
-export { ModalBundle } from './lib/ModalBundle';
+export { ModalBundle, useValues } from './lib/ModalBundle';
