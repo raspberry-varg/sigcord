d052e8a483a95b0ac780ecc2e66757fabaa66cb4
robertvargas-irq <90274287+robertvargas-irq@users.noreply.github.com>
2023-10-25 22:58:55 -0600

    Renderable: Created Renderable class

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export * from './lib/MenuView';
 export * from './lib/Router';
 export * from './lib/SmartComponents';
 export * from './lib/PrebuiltEmbeds';
+export * from './lib/Renderable';
 export { ModalBundle, useValues } from './lib/ModalBundle';
---
+++ src/lib/Renderable.ts
@@ -0,0 +1,63 @@
+import {
+  ActionRowBuilder,
+  EmbedBuilder,
+  Message,
+  MessageActionRowComponentBuilder,
+  RepliableInteraction
+} from "discord.js";
+import {safeRender} from "../util/RenderingUtil";
+import {MenuViewPayload} from "./MenuView";
+
+interface RenderOptions {
+  ephemeral: boolean;
+  replyToComponentOnFirstRender: boolean;
+}
+
+export class Renderable {
+  public message?: Message;
+
+  constructor(public interaction: RepliableInteraction, private readonly options: RenderOptions = {
+    ephemeral: true,
+    replyToComponentOnFirstRender: false,
+  }) {}
+
+  async triggerPreloads() {
+    if (!this.onLoad) return;
+    return this.onLoad();
+  }
+
+  messagePayload(): MenuViewPayload {
+    return {
+      embeds: this.embeds(),
+      ephemeral: this.options.ephemeral,
+      content: this.content(),
+      components: this.components(),
+    };
+  }
+  protected content(): string | undefined {
+    return undefined;
+  }
+  protected embeds(): EmbedBuilder[] | undefined {
+    return undefined;
+  }
+  protected components():
+    | ActionRowBuilder<MessageActionRowComponentBuilder>[]
+    | undefined {
+    return undefined;
+  }
+  protected async onLoad(): Promise<unknown> {
+    return undefined;
+  }
+
+  public async render() {
+    await this.triggerPreloads();
+
+    this.message = await safeRender(
+      this.interaction,
+      this.messagePayload(),
+      this.options.replyToComponentOnFirstRender
+    );
+    this.options.replyToComponentOnFirstRender = false;
+    return this.message;
+  }
+}
