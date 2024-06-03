42d905fd0491a371e8d5ac068ccea50c7384a8ca
Irii <90274287+robertvargas-irq@users.noreply.github.com>
2024-06-02 23:11:43 -0700

    debug now relies on development NODE_ENV

--- src/util/Logger.ts
+++ src/util/Logger.ts
@@ -1,17 +1,18 @@
 // TODO(@raspberry-varg): Use an actual logger
+export const debug = process.env.NODE_ENV === 'development';
 export const logger = {
   debug(msg: unknown, ...args: unknown[]) {
-    if (process.env.NODE_ENV === 'development') {
+    if (debug) {
       console.debug(msg, ...args);
     }
   },
   error(msg: unknown, ...args: unknown[]) {
-    if (process.env.NODE_ENV === 'development') {
+    if (debug) {
       console.error(msg, ...args);
     }
   },
   warn(msg: unknown, ...args: unknown[]) {
-    if (process.env.NODE_ENV === 'development') {
+    if (debug) {
       console.warn(msg, ...args);
     }
   },
