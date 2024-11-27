601cfabc27179be2cf51abe4ce393d6c4268f578
Irii <90274287+raspberry-varg@users.noreply.github.com>
2024-11-27 00:52:42 -0700

    export computed and read

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,7 @@ export {
   MaybeWritableSignal,
   Signal,
   Setter,
+  read,
   isSignal,
   isWritableSignal,
 } from './lib/Reactivity.js';
@@ -25,6 +26,7 @@ export {
   component,
   signal,
   writable,
+  computed,
   effect,
   embedEffect,
   componentEffect,
