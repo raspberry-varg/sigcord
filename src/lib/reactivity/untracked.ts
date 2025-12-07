2bc160ec2710032c1728af6331a65421840f8af1
Irii <90274287+raspberry-varg@users.noreply.github.com>
2025-12-07 01:43:51 -0700

    Untracked: Remove ad-hoc support for WritableSignal

--- src/lib/reactivity/untracked.ts
+++ src/lib/reactivity/untracked.ts
@@ -1,20 +1,10 @@
-import {
-  isWritableSignal,
-  createUntracked,
-  type WritableSignal,
-  type Signal,
-} from './core/signals.js';
+import { createUntracked, type Signal } from './core/signals.js';
 
 /**
  * Read a signal or callback of signals without subscribing it to the current
  * reactive context or effect.
  * @param signalOrFn Signal to read from or a function reading signals.
  */
-export function untracked<T>(
-  signalOrFn: (() => T) | Signal<T> | WritableSignal<T>,
-): T {
-  if (isWritableSignal(signalOrFn)) {
-    signalOrFn = signalOrFn.readonly();
-  }
+export function untracked<T>(signalOrFn: (() => T) | Signal<T>): T {
   return createUntracked(signalOrFn);
 }
