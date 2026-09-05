aafc73a51a9edc45cabb2acd6cf890570e939568
Irii <90274287+raspberry-varg@users.noreply.github.com>
2026-09-05 01:35:08 -0700

    If: Remove support for WritableSignal<T> to remove type inference of `Object |`

--- packages/jsx/src/controlFlow/if.ts
+++ packages/jsx/src/controlFlow/if.ts
@@ -1,15 +1,12 @@
 import {
   type Children,
-  HasWritableSignalStamp,
   type Owner,
   type Signal,
   ViewElementNode,
   type ViewNodeKind,
   type ViewNodeKindBase,
-  type WritableSignal,
   computed,
   isSignal,
-  isWritableSignal,
   owner,
   patchEffect,
   read,
@@ -17,10 +14,7 @@ import {
 } from '@sigcord/core';
 
 type Then<Condition, T_TRUE> = (
-  result: Condition extends
-    | Signal<infer C>
-    | WritableSignal<infer C>
-    | (() => infer C)
+  result: Condition extends Signal<infer C> | (() => infer C)
     ? Signal<NonNullable<C>>
     : NonNullable<Condition>,
 ) => T_TRUE;
@@ -34,41 +28,37 @@ interface WithChildren<Condition, T_TRUE> extends BaseProps<Condition> {
   children: Then<Condition, T_TRUE>;
 }
 
-interface WithAttributes<Condition, T_TRUE, T_FALSE>
-  extends BaseProps<Condition> {
+interface WithAttributes<
+  Condition,
+  T_TRUE,
+  T_FALSE,
+> extends BaseProps<Condition> {
   then: Then<Condition, T_TRUE>;
   else?: () => T_FALSE;
 }
 
 type IfProps<Condition, T_TRUE, T_FALSE> =
-  | WithChildren<Condition, T_TRUE>
-  | WithAttributes<Condition, T_TRUE, T_FALSE>;
+  WithChildren<Condition, T_TRUE> | WithAttributes<Condition, T_TRUE, T_FALSE>;
 
 export function If<Condition, T_TRUE, T_FALSE>(
-  props: IfProps<Condition, T_TRUE, T_FALSE>,
+  props: Readonly<IfProps<Condition, T_TRUE, T_FALSE>>,
 ): T_TRUE & T_FALSE extends Children<infer C>
   ? ViewElementNode<C> | C
   : ViewElementNode<ViewNodeKind> | ViewNodeKind {
+  const cond = props.cond;
   const then = 'then' in props ? props.then : props.children;
-
-  let resolved: unknown = props.cond;
-  if (HasWritableSignalStamp(resolved) && isWritableSignal(resolved)) {
-    resolved = resolved.readonly();
-  }
-
-  if (!isSignal(resolved)) {
+  if (!isSignal(cond)) {
     return untracked(() =>
-      resolved
-        ? then(resolved as Parameters<typeof then>[0])
+      cond
+        ? then(cond as Parameters<typeof then>[0])
         : 'else' in props
           ? props.else
           : null,
     ) as any;
   }
 
-  const truthy = computed(() => !!read<Condition>(resolved as Condition));
-
   const node = new ViewElementNode();
+  const truthy = computed(() => !!read<Condition>(cond as Condition));
   patchEffect(() => {
     let o: Owner | undefined;
     const res = truthy();
@@ -77,7 +67,7 @@ export function If<Condition, T_TRUE, T_FALSE>(
       o = owner(
         () =>
           untracked(() =>
-            then(resolved as Parameters<typeof then>[0]),
+            then(cond as Parameters<typeof then>[0]),
           ) as ViewNodeKindBase,
       );
     } else if ('else' in props) {
