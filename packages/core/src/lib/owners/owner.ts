530dc16c52d99d9314c3e6c7f70ab8e58014a8a7
Irii <90274287+raspberry-varg@users.noreply.github.com>
2026-09-09 20:09:41 -0700

    Owner: Add a lite version of owner in anticipation of decoupling owners from nodes

--- packages/core/src/lib/owners/owner.ts
+++ packages/core/src/lib/owners/owner.ts
@@ -4,20 +4,56 @@ import { ViewNode } from '../dom/viewNode.js';
 import type { ViewNodeKind, ViewNodeKindBase } from '../dom/viewNodeKind.js';
 import type { Recursive } from '../recursive.js';
 import { PatchTarget } from '../RenderingEngine.js';
-import type { DisposeFn, ResumeFn, SuspendFn } from './dispose.js';
-import { flattenToContentNodes } from './flattenToContentNodes.js';
-import { flatten } from './flatten.js';
 import type { DisposeFn, ResumeFn, SuspendFn } from '../render/dispose.js';
 import { flattenToContentNodes } from '../render/flattenToContentNodes.js';
 import { flatten } from '../render/flatten.js';
 import type { ContextNode } from '../contexts/contextNode.js';
 
+export interface Owner<
+  T extends ViewNodeKindBase = ViewNodeKindBase,
+> extends Disposable {
+  readonly root: ViewElementNode<T>;
+  readonly patchTarget?: PatchTarget;
+  readonly context?: ContextNode;
+  readonly parent: Owner | null;
+  readonly childOwners: Set<Owner>;
+  debugName?: string;
+  readonly disposed: boolean;
+  readonly suspended: boolean;
 
-export class Owner<T extends ViewNodeKindBase = ViewNodeKindBase>
-  implements Disposable
-{
-  readonly root = new ViewElementNode<T>();
+  getNodes(): readonly ViewNode<T>[];
+
+  registerNode(node: ViewNode<T>): void;
+
+  registerDisposal(disposal: DisposeFn): void;
+
+  registerComponentDisposal(id: string, disposal: DisposeFn): void;
+
+  registerOnSuspend(onSuspend: SuspendFn): void;
+
+  registerOnResume(onResume: ResumeFn): void;
+
+  addChild(owner: Owner<T>): void;
+
+  removeChild(owner: Owner<T>): void;
+
+  suspend(): void;
+
+  resume(): void;
+
+  flatten(): T[];
+
+  dispose(): void;
+
+  [Symbol.dispose](): void;
+}
+
+class OwnerImpl<
+  T extends ViewNodeKindBase = ViewNodeKindBase,
+> implements Owner<T> {
+  root = new ViewElementNode<T>();
   patchTarget?: PatchTarget;
+  context?: ContextNode;
   parent: Owner | null = null;
   childOwners: Set<Owner> = new Set<Owner>();
   debugName?: string;
@@ -29,8 +65,6 @@ export class Owner<T extends ViewNodeKindBase = ViewNodeKindBase>
   private readonly nodes: ViewNode<T>[] = [];
   private disposed_ = false;
   private suspended_ = false;
-
-  constructor() {}
 
   get disposed() {
     return this.disposed_;
@@ -107,6 +141,7 @@ export class Owner<T extends ViewNodeKindBase = ViewNodeKindBase>
   dispose() {
     if (this.disposed) return;
     this.disposed_ = true;
+    this.context = undefined;
 
     this.childOwners.forEach((owner) => owner.dispose());
     this.childOwners.clear();
@@ -163,24 +198,44 @@ export function setCurrentOwner(newOwner: Owner | null): Owner | null {
 export function owner<T extends ViewNodeKindBase>(
   ownerFn: () => ViewNodeKind<T> | void,
   patchTarget?: PatchTarget,
+  contextNode?: ContextNode,
 ): Owner<T> {
   logger.verbose(`creating a new owner with fn=${ownerFn}`);
-  const newOwner = new Owner<T>();
+
+  const root = new ViewElementNode<T>();
+  const newOwner = ownerLite<void>(
+    () => {
+      const content = ownerFn();
+      if (content) {
+        const nodes = flattenToContentNodes(
+          content as Recursive<T | ViewNode<T>>,
+        );
+        root.addChild(...nodes);
+      }
+    },
+    patchTarget,
+    contextNode,
+  );
+  (newOwner as OwnerImpl).root = root;
+  return newOwner as Owner<T>;
+}
+
+// TODO: Owner should be decoupled from the "DOM" tree.
+export function ownerLite<T>(
+  ownerFn: () => T,
+  patchTarget?: PatchTarget,
+  contextNode?: ContextNode,
+): Owner {
+  logger.verbose(`creating a new LITE owner with fn=${ownerFn}`);
+  const newOwner = new OwnerImpl();
   const prevOwner = setCurrentOwner(newOwner);
   newOwner.patchTarget = prevOwner?.patchTarget ?? patchTarget;
   newOwner.parent = prevOwner;
+  newOwner.context = contextNode ?? prevOwner?.context; // Reduce extra lookup loops.
   prevOwner?.addChild(newOwner);
 
-  let content;
   try {
-    content = ownerFn();
-    if (content) {
-      const root = newOwner.root;
-      const nodes = flattenToContentNodes(
-        content as Recursive<T | ViewNode<T>>,
-      );
-      root.addChild(...nodes);
-    }
+    ownerFn();
   } finally {
     setCurrentOwner(prevOwner);
   }
