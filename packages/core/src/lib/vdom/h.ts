a3752a4e6d16665fa7e77a3a7fb0db26214b347d
Irii <90274287+raspberry-varg@users.noreply.github.com>
2026-09-23 01:11:02 -0700

    h: Add overloads for functional components to defer their execution

--- packages/core/src/lib/vdom/h.ts
+++ packages/core/src/lib/vdom/h.ts
@@ -1,14 +1,24 @@
-import { type IntrinsicNode, NodeType } from './types.js';
+import { createDeferredNode } from './deferred.js';
+import { createIntrinsicNode } from './intrinsic.js';
+import { type DeferredNode, type IntrinsicNode } from './types.js';
 
 import type { IntrinsicPropsMap } from './props.js';
 
+/**
+ * Hyper-script signature to create a new intrinsic node.
+ */
 export function h<K extends keyof IntrinsicPropsMap>(
   type: K,
-  props: IntrinsicPropsMap[K],
-): IntrinsicNode {
-  return {
-    $$typeof: NodeType.Intrinsic,
-    type: type,
-    props: (props as Record<string, unknown>) ?? {},
-  };
+  props?: IntrinsicPropsMap[K] | null,
+): IntrinsicNode;
+
+/**
+ * Hyper-script signature to defer execution of a functional component.
+ */
+export function h<P>(type: (props: P) => unknown, props?: P | null): DeferredNode;
+export function h(type: any, props: Record<string, unknown>): IntrinsicNode | DeferredNode {
+  const finalProps = props ?? {};
+  return typeof type === 'function'
+    ? createDeferredNode(type, finalProps)
+    : createIntrinsicNode(type, props);
 }
