8279e1f6896181f42081d0df52d474ac1a79076e
Irii <90274287+raspberry-varg@users.noreply.github.com>
2026-09-23 20:13:29 -0700

    Builtins: Mark navigation functions as deprecated

--- packages/core/src/lib/builtins/builtins.ts
+++ packages/core/src/lib/builtins/builtins.ts
@@ -55,6 +55,8 @@ export const getNextUniqueComponentId: Synapse['getNextUniqueComponentId'] = () 
 // Navigation
 
 /**
+ * @deprecated Please use {@link useNavigation().push()}.
+ *
  * Instantiate and navigate to a different view.
  *
  * - Can navigate back out of the view using {@link goBack}
@@ -71,6 +73,8 @@ export const goTo: Synapse['goTo'] = (view, props) => {
 };
 
 /**
+ * @deprecated Please use {@link useNavigation().pop()}.
+ *
  * Navigate back to the calling view.
  *
  * @throws If not navigated to using {@link goTo}
@@ -87,6 +91,8 @@ export const goBack: Synapse['goBack'] = () => {
 };
 
 /**
+ * @deprecated Please use {@link useNavigation().canGoBack()}.
+ *
  * Returns true if this view was navigated to using {@link goTo}. Safely allows
  * the use of {@link goBack} since the previous menu is on the navigation stack.
  */
